11fed3b427a68c586ddab65b4aeedfe11b7709e9
“Wahid7852” <demolishercrazy@gmail.com>
2022-03-04 17:59:29 +0530

    stable build 4.14.266

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Mar 4 00:12:31 IST 2022"
-#define LINUX_COMPILE_BY "AbdulWahid"
-#define LINUX_COMPILE_HOST "PC"
-#define LINUX_COMPILER "DragonTC clang version 10.0.0-20190810 (based on LLVM 10.0.0svn), LLD 10.0.0 (/home/najahi/dtc/llvm-project/lld 14de7f29eb5ed461cfbe43e304299710a8bb212e)"
